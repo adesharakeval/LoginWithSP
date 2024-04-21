b98804fc35887ccb83dd8f2f6e8e8a967b7e36c9
adesharakeval <adesharakeval@gmail.com>
2024-04-21 15:21:48 +0530

    code commiting

---
+++ LoginWithSP/WebForm1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace LoginWithSP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("sp_Login",con);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = con;
+
+            cmd.Parameters.AddWithValue("@Username", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@Password", TextBox2.Text);
+            con.Open();
+            int usercount = (Int32)cmd.ExecuteScalar();
+            if (usercount == 1)
+            {
+                Response.Redirect("Welcome.aspx");
+            }
+            else
+            {
+                con.Close();
+                Label1.Text = "Invalid UserName or Password";
+            }
+        }
+    }
+}
---
+++ LoginWithSP/Welcome.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Welcome.aspx.cs" Inherits="LoginWithSP.Welcome" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    Hey welcome 
+    </div>
+    </form>
+</body>
+</html>
